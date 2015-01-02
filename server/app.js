f51999a963c85b933f32c9070a20ebf0f4c52e31
Sven Mkw <sven.mkw@gmail.com>
2015-01-02 12:39:36 +0100

    js core enhancements should load first

--- server/app.js
+++ server/app.js
@@ -7,15 +7,17 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+// apply JS core enhancements
+require('./libs/improve')();
+
 var express = require('express');
 var config = require('./config/environment');
+
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
-
-require('./libs/improve')();
 
 // Start server
 server.listen(config.port, config.ip, function () {

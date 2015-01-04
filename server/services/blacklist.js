45dbaf8956f68473fbfaf7d88a3769a42dd4185e
Sven Mkw <sven.mkw@gmail.com>
2015-01-04 14:14:49 +0100

    blacklisting

--- server/services/blacklist.js
+++ server/services/blacklist.js
@@ -1,4 +1,37 @@
 
+var _ = require('lodash');
+
+/** SETUP BLACKLIST **/
+
+var blacklist = require('../../data/blacklist');
+validateBlacklist();
+
+
+/** HELPERS **/
+
+function validateBlacklist() {
+  blacklist = _.map(blacklist, function(entry) {
+    return validateStringForBlacklist(entry);
+  });
+}
+
+function validateStringForBlacklist(entry) {
+  return entry.toLowerCase().trim();
+}
+
+/** EXPORTS **/
+
 exports.contains = function(label) {
+  var validated = validateStringForBlacklist(label);
+
+  for (var i = 0; i < blacklist.length; i++) {
+    var entry = blacklist[i];
+
+    if (validated.contains(entry)) {
+      return true;
+    }
+  }
+
   return false;
 }
+

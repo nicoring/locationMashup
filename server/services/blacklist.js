3d1a985c9078f2eb1de5309cbc5eb1770e8cc69c
Sven Mkw <sven.mkw@gmail.com>
2015-01-21 23:37:16 +0100

    enhanced blacklist by type filter

--- server/services/blacklist.js
+++ server/services/blacklist.js
@@ -4,8 +4,6 @@ var _ = require('lodash');
 /** SETUP BLACKLIST **/
 
 var blacklist = require('../../data/blacklist');
-// validateBlacklist();
-
 
 /** HELPERS **/
 
@@ -15,23 +13,45 @@ function validateBlacklist() {
   });
 }
 
+/**
+ * Return true, if type is in entryTypes.
+ * Consider no types as disabled type filtering.
+ */
+function containsType(entryTypes, type) {
+  return entryTypes.length === 0 || !!_.find(entryTypes, type);
+}
+
 function validateStringForBlacklist(entry) {
   return entry.toLowerCase().trim();
 }
 
 /** EXPORTS **/
 
-exports.contains = function(label) {
-  var validated = validateStringForBlacklist(label);
+exports.contains = function(label, type) {
+
+  var validated = validateStringForBlacklist(label),
+      entry;
 
   for (var i = 0; i < blacklist.length; i++) {
-    var entry = blacklist[i];
+    entry = blacklist[i];
 
-    if (validated.contains(entry)) {
+    if (typeof entry === 'string') {
+      entry = {
+        label: entry,
+        types: []
+      };
+    }
+
+    if (!entry.types) {
+      entry.types = [];
+    }
+
+    if (containsType(entry.types, type) && validated.contains(entry.label)) {
       return true;
     }
   }
 
   return false;
+
 }
 

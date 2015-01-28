8ad3e1d71a759d06f1cd0ac46b851dca935785ff
Sven Mkw <sven.mkw@gmail.com>
2015-01-28 20:31:27 +0100

    refactored details controller - added details service

--- client/app/details/details.controller.js
+++ client/app/details/details.controller.js
@@ -1,78 +1,10 @@
 'use strict';
 
-var typeMapping = {
-  'http://dbpedia.org/ontology/Restaurant': 'Restaurant',
-  'http://protege.cim3.net/file/pub/ontologies/travel/travel.owl#Sightseeing': 'Sightseeing',
-  'http://wafi.iit.cnr.it/angelica/Hontology.owl#PointsOfInterest': 'PointsOfInterest',
-  'http://purl.org/acco/ns#Accommodation': 'Accommodation',
-  'http://wafi.iit.cnr.it/angelica/Hontology.owl#Accommodation': 'Accommodation',
-  'http://purl.org/goodrelations/v1#ProductOrService': 'ProductOrService'
-};
-
-var tourpediaToWikivoyageMapping = {
-  'Restaurant': ['DrinkActivity', 'DoActivity', 'SeeActivity', 'BuyActivity', 'Activity'],
-  'Sightseeing': ['DrinkActivity', 'EatActivity', 'DoActivity', 'SeeActivity', 'BuyActivity', 'Activity'],
-  'PointsOfInterest': ['DrinkActivity', 'EatActivity', 'DoActivity', 'SeeActivity', 'BuyActivity', 'Activity'],
-  'Accommodation': ['DrinkActivity', 'EatActivity', 'DoActivity', 'SeeActivity', 'BuyActivity', 'SleepActivity', 'Activity'],
-  'ProductOrService': ['DrinkActivity', 'EatActivity', 'Activity']
-};
-
-function getHashTag(string) {
-  return string.split('#')[1];
-}
-
-function selectRandomPlacesByType(places, type) {
-
-  // get type of selected place and get matching wikivoyage Activities based on the mapping above
-  var categories = tourpediaToWikivoyageMapping[ typeMapping[type] ],
-      elementType;
-
-  return _(places)
-    .filter(function (el) {
-      elementType = getHashTag(el.type);
-      return _.find(categories, function (category) {
-        return elementType === category;
-      });
-    })
-    .shuffle()
-    .slice(0, 6)
-    .value();
-}
-
-function getDistrictOfPlaces(places) {
-
-  var placeCounts = {},
-      mostProbablyDistrict = '',
-      bestCount = -1,
-      location;
-
-  _.forEach(places, function (place) {
-    location = place.location;
-    if (placeCounts[location] !== null) {
-      placeCounts[location] += 1;
-    } else {
-      placeCounts[location] = 1;
-    }
-  });
-
-  _.forEach(placeCounts, function (count, location) {
-    if (count > bestCount) {
-      bestCount = count;
-      mostProbablyDistrict = location;
-    }
-  });
-
-  return mostProbablyDistrict;
-
-}
-
 angular.module('locationMashupApp')
-  .controller('DetailsCtrl', function ($scope, $stateParams, $http, $state, $location) {
+  .controller('DetailsCtrl', function ($scope, $stateParams, $http, $state, $location, Details) {
 
     var id = $stateParams.id,
-        interestingPlaces = [],
-        userLocation;
-
+        interestingPlaces = [];
 
     // lodash range in scope
     $scope.range = _.range;
@@ -174,124 +106,37 @@ angular.module('locationMashupApp')
 
     /** dispatch all ajax requests **/
 
-    getDetails(id);
-    getReviews(id);
-    getImageUrl(id);
+    Details.collectDetails(id).then(function(place) {
 
+      // TODO: refactor html template to use $scope.details directly
+      $scope.details = place;
+      $scope.hasLabel = $scope.details.hasLabel;
+      $scope.detailsMap.cetner = $scope.details.position;
+      $scope.locationInfo = $scope.details.districtInfo; // when refactoring rename locationInfo to districtInfo!!
 
-    function getDetails(placeId) {
+      // marker position in wikivoyage map should not update
+      // thus make a deep copy
+      $scope.mainMarker.coords = _.cloneDeep($scope.details.position);
 
-      $http.get('/api/placeDetails/' + placeId)
-        .success(function(place) {
+      // prepare marker models
+      $scope.detailsMarkers = _.map($scope.details.interestingPlaces, function (el, i) {
+        return {
+          id: i,
+          resource: el.s,
+          latitude: el.lat,
+          longitude: el.long,
+          title: el.label,
+        };
+      });
 
-          // just store one label
-          if ('label' in place) {
-            $scope.hasLabel = true;
-          } else if ('rdfsLabel' in place) {
-            place.label = place.rdfsLabel;
-            $scope.hasLabel = true;
-          } else if ('fn' in place) {
-            place.label = place.fn;
-            $scope.hasLabel = true;
-          }
+    });
 
-          $scope.details = place;
+    Details.getReviews(id).then(function(reviews) {
+      $scope.reviews = reviews;
+    });
 
-          var position = {
-            latitude: place.lat,
-            longitude: place.lng
-          };
-
-          // marker position in wikivoyage map should not update
-          // thus make a deep copy
-          $scope.mainMarker.coords = _.cloneDeep(position);
-
-          // set the center of the map
-          $scope.detailsMap.center = position;
-
-          // after loading the place details, get interesting places around
-          getInterestingPlaces(place.lat, place.lng, place.type);
-        })
-        .error(function() {
-          console.error('details loading failed');
-        });
-
-    }
-
-    function getReviews(placeId) {
-
-      $http.get('/api/placeDetails/reviews/' + placeId)
-        .success(function(reviews) {
-          reviews = _.filter(reviews, function (review) {
-            return (review.language === 'en' || review.language === 'de') && review.wordsCount < 50;
-          });
-
-          $scope.reviews = reviews;
-        })
-        .error(function(error) {
-          console.error('reviews loading failed', error);
-        });
-
-    }
-
-    function getImageUrl(placeId) {
-
-      $http.get('/api/placeDetails/image/' + placeId)
-        .success(function(data) {
-          $scope.imgUrl = data.url;
-        })
-        .error(function(error) {
-          console.error('image loading failed', error);
-        });
-
-    }
-
-    function getInterestingPlaces(lat, lng, type) {
-
-      var url = '/api/placeDetails/places?lat=' + lat + '&lng=' + lng;
-      $http.get(url)
-        .success(function (wikivoyagePlaces) {
-
-          interestingPlaces = selectRandomPlacesByType(wikivoyagePlaces.result, type);
-          $scope.detailsMarkers = _.map(interestingPlaces, function (el, i) {
-            return {
-              id: i,
-              resource: el.s,
-              latitude: el.lat,
-              longitude: el.long,
-              title: el.label,
-            };
-          });
-
-          getLocationInfo(wikivoyagePlaces.result);
-        })
-        .error(function (error) {
-          console.log('places loading failed', error);
-        });
-    }
-
-    function getLocationInfo(places) {
-
-      var locationURI = '',
-          url;
-
-      if (places.length > 0) {
-        locationURI = getDistrictOfPlaces(places);
-      } else {
-        locationURI = 'http:localhost/wikivoyage/Berlin';
-      }
-
-      url = '/api/placeDetails/locationInfo?location=' + encodeURI(locationURI);
-      $http.get(url)
-        .success(function (data) {
-          if (data.length > 0) {
-            $scope.locationInfo = data[0];
-          }
-        })
-        .error(function (error) {
-          console.log('locationInfo loading failed', error);
-        });
-
-    }
+    Details.getImageUrl(id).then(function(url) {
+      $scope.imgUrl = url;
+    });
 
   });

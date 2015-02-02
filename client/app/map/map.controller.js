c2052669b189ab8f8883d59ba21d69ec153ca990
Sven Mkw <sven.mkw@gmail.com>
2015-02-02 17:50:48 +0100

    added average rating calculation

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -66,6 +66,25 @@ angular.module('locationMashupApp')
     // img from foursquare or google places
     $scope.imgUrl = '';
     $scope.hasImage = false;
+    $scope.averageRating = 0;
+
+    /** get average star rating for a selected place **/
+
+    function averageStarRating(placeId) {
+      return Details.getReviews(placeId).then(function(reviews) {
+        var ratings = _(reviews)
+          .pluck('rating')
+          .filter(function(rating) { return rating > 0; });
+
+        var sum = ratings.reduce(function(result, rating) { return result + rating; }, 0);
+        var count = ratings.value().length;
+        if (count > 0) {
+          return sum / count;
+        } else {
+          return 0;
+        }
+      });
+    }
 
     $scope.markerClicked = function(marker) {
       var id = marker.model.id;
@@ -73,15 +92,20 @@ angular.module('locationMashupApp')
       $scope.hasImage = false;
       $scope.imgUrl = '';
 
+      averageStarRating(id).then(function(averageRating) {
+        $scope.averageRating = averageRating;
+      });
+
       placeDfd.then( function(placeDetails) {
         placeDetails.id = id;
         $scope.selectedPlace = placeDetails;
         $scope.isMarkerSelected = true;
 
-        photo.getPhotoForUrl(placeDetails.wikiLink).done( function(photoUrl) {
-          $scope.imgUrl = photoUrl;
-          $scope.hasImage = true;
-        });
+        photo.getPhotoForUrl(placeDetails.wikiLink)
+          .done( function(photoUrl) {
+            $scope.imgUrl = photoUrl;
+            $scope.hasImage = true;
+          })
       });
     };
 
